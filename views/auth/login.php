973e121aaa7107859042bce89317d32ebb841155
DoHoan06 <quyhoanfk123@gmail.com>
2025-12-03 09:24:24 +0700

    update

--- views/auth/login.php
+++ views/auth/login.php
@@ -105,7 +105,7 @@ $content = ob_get_clean();
 
 // Hiển thị layout auth với nội dung
 view('layouts.AuthLayout', [
-    'title' => $title ?? 'Đăng nhậphh',
+    'title' => $title ?? 'Đăng nhập',
     'content' => $content,
     'extraJs' => ['js/login.js'],
 ]);
